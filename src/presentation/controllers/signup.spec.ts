5f8a5f197b33632fc046693524c248362be89a90
Thiago Soares <thiago.soares@dasa.com.br>
2023-12-18 18:38:08 -0300

    test: add controller test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,5 +1,16 @@
+import { SignUpController } from './signup'
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    expect(1).toBe(1)
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
